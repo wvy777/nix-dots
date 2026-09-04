9d7c03a5b3b55225ce72c7cf7130b1cab29af8c6
kaan-w <105508453+kaan-w@users.noreply.github.com>
2026-09-05 02:48:39 +0300

    ddnet: improvements
    - use skins.ddstats.tw for skins
    - better assets
    - disable predict events

--- features/gui/ddnet/default.nix
+++ features/gui/ddnet/default.nix
@@ -7,24 +7,9 @@
     home.file = let
       ddnet_dir = ".local/share/ddnet";
 
-      kir4i_contrast = pkgs.fetchurl {
-        url = "https://skins.ddstats.tw/kir4i_contrast.png";
-        hash = "sha256-qmmko+wrM0H7YMVU4/+z+EVHS7zBFPwGHXuTUt53YFI=";
-      };
-
-      kir4i_k = pkgs.fetchurl {
-        url = "https://skins.ddstats.tw/kir4i_k.png";
-        hash = "sha256-xWwkmXfjdciKTO9yyV6Mqfu677U2gj0Lq82UG6KQsqY=";
-      };
-
-      black_grey = pkgs.fetchurl {
-        url = "https://teedata.net/databasev2/entities/black_grey/black_grey.png";
-        hash = "sha256-ryJ11161ockZIIx2dUk053hDmDD45BMEIWyOZhCRj2Y=";
-      };
-
-      black_hud = pkgs.fetchurl {
-        url = "https://teedata.net/databasev2/templates/black_hud/black_hud.png";
-        hash = "sha256-rernFq5yfQpaj4KKHOaLJLbRX/A8ZlFSAC6KfQ0UOUU=";
+      akka2 = pkgs.fetchurl {
+        url = "https://teedata.net/databasev2/entities/akka2/akka2.png";
+        hash = "sha256-t+7Ns73CSnHAxwSOJYCfRtmmKZbCUzNpVND2eOmkpos=";
       };
 
       kiri = pkgs.fetchurl {
@@ -32,16 +17,19 @@
         hash = "sha256-p4eJvi4EL6VfoFY91MPwdp3SJkekz1vWFJZeZWA9tVc=";
       };
 
+      schichimiya = pkgs.fetchurl {
+        url = "https://teedata.net/databasev2/emoticons/schichimiya/schichimiya.png";
+        hash = "sha256-aTP60Z0LrRLgQ3QwBpl9EKd7aqnyNRkqpci9uaBdmak=";
+      };
+
       silhouette_particles = pkgs.fetchurl {
         url = "https://teedata.net/databasev2/particles/silhouette_particles/silhouette_particles.png";
         hash = "sha256-wVaUnEn43+Dm0HfoXrSb+dD8m8csCt/HRqHHKpFfkA4=";
       };
     in {
-      "${ddnet_dir}/skins/kir4i_contrast.png".source = kir4i_contrast;
-      "${ddnet_dir}/skins/kir4i_k.png".source = kir4i_k;
-      "${ddnet_dir}/assets/entities/black_grey.png".source = black_grey;
-      "${ddnet_dir}/assets/hud/black_hud.png".source = black_hud;
+      "${ddnet_dir}/assets/entities/akka2.png".source = akka2;
       "${ddnet_dir}/assets/game/kiri.png".source = kiri;
+      "${ddnet_dir}/assets/emoticons/schichimiya.png".source = schichimiya;
       "${ddnet_dir}/assets/particles/silhouette_particles.png".source = silhouette_particles;
     };
 
